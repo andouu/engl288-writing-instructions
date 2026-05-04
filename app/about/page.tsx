420f13f5e90b70e5f49758eef09821f4b2c6a25c
Andrew Zhou <andrewzhou16@gmail.com>
2026-05-03 21:12:14 -0700

    Add: About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+import { Section } from "../components/TutorialComponents";
+
 export default function About() {
-  return "About"
+  return <div className="py-15">
+    <Section heading="About">
+      <p>This website was created by Andrew Zhou for Engl 288. The code is open-sourced and can be found at this <Link href="https://github.com/andouu/engl288-writing-instructions" target="_blank" className="underline">GitHub repo</Link>.</p>
+    </Section>
+  </div>
 }
