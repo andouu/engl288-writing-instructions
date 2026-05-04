f94b73a2811d9125650c1960111b0cdcb3e53bf7
Andrew Zhou <andrewzhou16@gmail.com>
2026-05-03 21:16:23 -0700

    Add: Favicon, slight changes to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ import { Section } from "../components/TutorialComponents";
 export default function About() {
   return <div className="py-15">
     <Section heading="About">
-      <p>This website was created by Andrew Zhou for Engl 288. The code is open-sourced and can be found at this <Link href="https://github.com/andouu/engl288-writing-instructions" target="_blank" className="underline">GitHub repo</Link>.</p>
+      <p>This website was created by Andrew Zhou for Engl 288. The code is open-sourced and can be found at this <Link href="https://github.com/andouu/engl288-writing-instructions" target="_blank" className="underline font-medium">GitHub repo</Link>.</p>
     </Section>
   </div>
 }
